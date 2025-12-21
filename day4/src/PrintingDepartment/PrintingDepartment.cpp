297fd8b7d23cc5b9cc330f699663f023ce74f860
Viktor <viklin99@gmail.com>
2025-12-21 16:25:40 +0100

    Added function for string->bool on input

--- day4/src/PrintingDepartment/PrintingDepartment.cpp
+++ day4/src/PrintingDepartment/PrintingDepartment.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstddef>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 
@@ -14,10 +15,10 @@ PrintingDepartment::PrintingDepartment() {
 /*
  * Solution for problem 1
  */
-int PrintingDepartment::solve(std::vector<std::string>& banks) {
+int PrintingDepartment::solve(std::vector<std::string>& paper) {
 
     int sum = 0;
-
+    getMatrix(paper);
     return sum;
 }
 
@@ -25,7 +26,7 @@ int PrintingDepartment::solve(std::vector<std::string>& banks) {
 /*
  * Solution for problem 2
  */
-long PrintingDepartment::solve2(std::vector<std::string>& banks) {
+long PrintingDepartment::solve2(std::vector<std::string>& paper) {
 
     long sum = 0;
 
@@ -33,6 +34,39 @@ long PrintingDepartment::solve2(std::vector<std::string>& banks) {
 }
 
 
+/*
+ * Transfors the input string into a matrix. True indicates presence of paper
+ */
 std::vector<std::vector<bool>> PrintingDepartment::getMatrix(std::vector<std::string>& input) {
-    return {};
+    auto ret = std::vector<std::vector<bool>>();
+
+    // Extract all paper positions
+    for(auto& str : input) {
+        ret.insert(ret.end(), std::vector<bool>());
+
+        // Use lambda function for transforming a text matrix to a boolean one
+        std::transform(str.begin(), str.end(),std::back_inserter(ret[ret.size()-1]),
+        [](char c) -> bool {
+            return c=='@';
+        });
+    }
+
+    return ret;
+}
+
+
+/*
+ * Print vectors
+ */
+void PrintingDepartment::printMatrix(std::vector<std::vector<bool>> matrix) {
+    
+    std::cout << std::endl;
+
+    for(auto& vec : matrix) {
+        for(bool b : vec) {
+            std::cout << (b ? "@" : ".");
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 }
--- day4/src/PrintingDepartment/PrintingDepartment.h
+++ day4/src/PrintingDepartment/PrintingDepartment.h
@@ -9,13 +9,14 @@ class PrintingDepartment {
         PrintingDepartment();
 
         // Solution to first problem
-        int solve(std::vector<std::string>& banks);
+        int solve(std::vector<std::string>& paper);
 
         //Solution to second problem
-        long solve2(std::vector<std::string>& banks);
+        long solve2(std::vector<std::string>& paper);
     private:
         // Helper functions
         std::vector<std::vector<bool>> getMatrix(std::vector<std::string>& input);
+        void printMatrix(std::vector<std::vector<bool>> matrix);
 };
 
 #endif
