295da8d7fd11b603ea1267b2303c6bb92e96b1d5
Viktor <viklin99@gmail.com>
2025-12-21 11:01:11 +0100

    Solution to day3p1

--- day3/src/BatteryBank/BatteryBank.cpp
+++ day3/src/BatteryBank/BatteryBank.cpp
@@ -14,6 +14,10 @@ int BatteryBank::solve(std::vector<std::string>& banks) {
 
     int sum = 0;
 
+    for(std::string& bank : banks) {
+        sum += findLargestJoltage(bank);
+    }
+
     return sum;
 }
 
@@ -28,9 +32,25 @@ int BatteryBank::solve2(std::vector<std::string>& banks) {
 
 
 /*
- * Handle the id string.
+ * Find the largest Joltage possible in string.
  */
 int BatteryBank::findLargestJoltage(std::string& bankString) {
+    
+    // {index, value]
+    int largest = 0;
+    int largestFollowing = -1;
+    int secondLargest = 0;
 
-    return 0;
+    for(int i = 0; i < bankString.length(); i++) {
+
+        if(bankString[i]-'0' > largestFollowing) largestFollowing = bankString[i]-'0';
+
+        if(bankString[i]-'0' > largest) {
+            secondLargest = largest;
+            largest = bankString[i]-'0';
+            largestFollowing = -1;
+        }
+    }
+
+    return largestFollowing == -1 ? (secondLargest*10 + largest) : (largest*10 + largestFollowing);
 }
